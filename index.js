a7ef32951e46d7d79550d8bbbba635e7f4769e5d
Saul Graves <graves.saul@gmail.com>
2021-01-25 12:57:33 -0500

    add data.json, require data in index.js, pass data to and use in subreddit.js, loop through posts

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+console.log(redditData)
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-
 
 app.get('/', (req, res) => {
     res.render(`home`);
@@ -12,10 +13,9 @@ app.get('/', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    const cities = ['New York City', 'Pittsburgh', 'Madrid', 'Tamarindo'];
-    res.render('subreddit', { subreddit, cities });
+    const data = redditData[subreddit];
+    res.render('subreddit', { ...data })
 });
-
 
 app.listen(8080, () => {
     console.log('LISTENING ON PORT 8080');
